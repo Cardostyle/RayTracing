286dac775f2756917db659b1b91fb4bd073ac865
Hoppe <ricardo.hoppe@student.hs-anhalt.de>
2023-10-16 13:03:21 +0200

    Initial commit

---
+++ src/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+
+    @Test
+    void creatingRay(){
+
+        assertEquals(1,1);
+        assertEquals(1,1);
+    }
+}
