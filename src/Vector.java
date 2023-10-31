6c9a01cc86da5e3d4e4b5663576c101f4f304340
Cardostyle <cardostyle19@gmail.com>
2023-10-31 15:19:56 +0100

    VectorMath in Point und Vector klasse überschrieben

--- src/Vector.java
+++ src/Vector.java
@@ -26,4 +26,63 @@ public class Vector {
     public double getW() {
         return w;
     }
+
+    public Vector add(Vector v) {
+        return new Vector(x + v.x, y + v.y, z + v.z);
+    }
+
+    public Vector sub(Vector v) {
+        return new Vector(x - v.x, y - v.y, z - v.z);
+    }
+
+    public Vector negate() {
+        return new Vector(-x, -y, -z);
+    }
+
+    public Vector mult(double n) {
+        return new Vector(x * n, y * n, z * n);
+    }
+
+    public Vector div(double n) {
+        return new Vector(x / n, y / n, z / n);
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector normalized() {
+        double mag = magnitude();
+        return this.div(mag);
+    }
+
+    public double dot(Vector v) {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    public Vector cross(Vector v) {
+        return new Vector(
+                y * v.z - z * v.y,
+                z * v.x - x * v.z,
+                x * v.y - y * v.x
+        );
+    }
+
+    @Override
+    public String toString(){
+        return "x: "+x+ "; y: "+y+"; z: "+z;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vector other)
+        {
+            return Math.abs(this.x - other.x) < 0.00001 &&
+                    Math.abs(this.y - other.y) < 0.00001 &&
+                    Math.abs(this.z - other.z) < 0.00001 &&
+                    Math.abs(this.w - other.w) < 0.00001;
+        }
+        return false;
+    }
+
 }
